ad651d6a5f2397719fd7adbcb16774eac7147deb
RahimunnisaMohammad <144161321+RahimunnisaMohammad@users.noreply.github.com>
2023-11-10 15:22:07 +0530

    Update selection sort.c
    soooo beautiful soo elegant
    just looking like a wowwwwwwwwwwwwwwwwwwwwww

--- selection sort.c
+++ selection sort.c
@@ -1,20 +1,9 @@
-#include<stdio.h>
-int main()
-{
-	int a[10],n=5,i,j,min,teA;
-	for(i=0;i<n;i++)
-	scanf("%d",&a[i]);
-	for(i=0;i<n;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-			if(a[i]<a[j])
-			{
-				teA=a[j];
-				a[j]=a[i];
-				a[i]=teA;
-			}
-		}
-	}
-	NAA ISTAM REYYY MALLI NAA ISTAM
-}
+nenu jadestha chudu
+nenu jadestha chudu
+na jada sayi chudakunte yeesestha chuudu
+jambalakadi jaaru mithaaya 
+
+
+
+annna nen malla ochchina anna
+ee raithu bidda
